0f9098e8b412d2dbcc0031b15cc82d837607fc55
unknown <sisiphopepengweni@gmail.com>
2023-07-03 15:31:47 +0200

    about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+   <div>
+    <img src="https://i.postimg.cc/15B8z9gs/IMG-3453-removebg-preview.png" alt="image">
+   </div>
+   <div>
+     <h1>ABOUT ME</h1>
+    <p>  I go by the name Sisipho Pepengweni. I was born an bred in a small town called 
+Cofimvaba in the Eastern Cape. I currently reside in Lower crosssroad, a small 
+Township in Cape Town. I am an aspiring full stack developer who's interest is 
+enhancing my professional skills and gaining professional experience in the field.
+I spend most of my time on computers because I am intrigued by how these 
+machines work. My love for coding developed when I attended a career exhibition
+at UCT.
+</p>
+   </div>
   </div>
 </template>
+
+<style scoped>
+.about {
+  background-color: #E2CBB9;
+}
+</style>
+  
+
